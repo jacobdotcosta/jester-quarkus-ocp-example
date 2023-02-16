b38dcbb407ad17a04af797e7d697f636623ab4d4
Antonio Costa <a.costa@trikorasolutions.com>
2023-02-16 10:48:51 +0100

    Separate Unit and IT tests

--- src/test/java/org/acme/GreetingResourceOpenShiftIT.java
+++ src/test/java/org/acme/GreetingResourceOpenShiftIT.java
@@ -4,6 +4,6 @@ import io.jester.api.OpenShiftServiceConfiguration;
 import io.jester.api.RunOnOpenShift;
 
 @RunOnOpenShift
-@OpenShiftServiceConfiguration(forService = "app", useInternalService = true)
+@OpenShiftServiceConfiguration(forService = "app", useInternalService = false)
 public class GreetingResourceOpenShiftIT extends GreetingResourceIT {
 }
